367752591716a06205fea1330639a0c36508b113
Ailing Zhang <ailzhang@fb.com>
2020-09-15 23:50:45 -0700

    Use iterator of DispatchKeySet.
    ghstack-source-id: 831499a0228af9b23bd82dbc8084be3cb2e39600
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/44682

--- c10/core/DispatchKeySet.h
+++ c10/core/DispatchKeySet.h
@@ -186,13 +186,11 @@ public:
 C10_API std::string toString(DispatchKeySet);
 C10_API std::ostream& operator<<(std::ostream&, DispatchKeySet);
 
+// Resolve alias dispatch key Autograd
+C10_API DispatchKeySet getRuntimeAutogradKeySet();
+
 // Resolve alias dispatch key to DispatchKeySet if applicable
 C10_API DispatchKeySet getRuntimeDispatchKeySet(DispatchKey t);
-
-// TODO(#43441): Once we have iterator-like funtionality on DispatchKeySet
-//      we can remove this API and use c10::getRuntimeDispatchKeySet instead.
-//      This API is only used in aten/src/ATen/core/dispatch/OperatorEntry.cpp.
-C10_API ArrayRef<DispatchKey> getRuntimeDispatchKeys(DispatchKey k);
 
 // This API exists because we have a use case for checking
 // getRuntimeDispatchKeySet(alias).has(DispatchKey::Undefind)
@@ -212,6 +210,6 @@ static inline DispatchKey legacyExtractDispatchKey(DispatchKeySet s) {
   // is the most likely key that will need this treatment;
   // After Autograd keys are moved from globally enabled set to TensorImpl,
   // we should remove all Autograd keys before taking highestPriority.
-  return (s - getRuntimeDispatchKeySet(DispatchKey::Autograd)).highestPriorityTypeId();
+  return (s - getRuntimeAutogradKeySet()).highestPriorityTypeId();
 }
 }
